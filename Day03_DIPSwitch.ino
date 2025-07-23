177cd9d10534de77db131f830f737e7bb10cb2ed
Datamuch3r <jonathan.lara222@icloud.com>
2025-07-22 21:35:22 -0700

    Day03_DIPSwitch.ino

--- Day03_DIPSwitch.ino
+++ Day03_DIPSwitch.ino
@@ -1,11 +1,17 @@
-#include "arduino_secrets.h"
+#include "Arduino.h"  // include information about our HERO
+
+#define CABIN_LIGHTS_PIN 12         // Control our lander's lights using HERO's pin 12
+#define CABIN_LIGHTS_SWITCH_PIN 2   // Connect our DIP switch to pin 2
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(CABIN_LIGHTS_PIN, OUTPUT);      // Set light pin as OUTPUT
+  pinMode(CABIN_LIGHTS_SWITCH_PIN, INPUT); // Set switch pin as INPUT
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (digitalRead(CABIN_LIGHTS_SWITCH_PIN) == HIGH) {
+    digitalWrite(CABIN_LIGHTS_PIN, HIGH); // If switch is ON, turn light ON
+  } else {
+    digitalWrite(CABIN_LIGHTS_PIN, LOW);  // If switch is OFF, turn light OFF
+  }
 }
